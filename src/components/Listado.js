4c2292de045829ef2b5d821f82d2e1f35e1970ba
Mary <pumaisabel029@gmail.com>
2022-12-30 21:44:38 -0300

    validacion de la autenticacion del usuario

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,19 @@
-function Listado () {
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+function Listado() {
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        
+        const token = localStorage.getItem('token');
+        console.log(token);
+        if (token === null) {
+            navigate('/');
+        }
+        
+    }, [navigate]);
+   
     return (
         <h2>soy el componente listado</h2>
     )
