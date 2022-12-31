31d8bddbc92e9db6288c51619155fe9670226949
Mary <pumaisabel029@gmail.com>
2022-12-31 12:08:28 -0300

    creacion del footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+
+    <Footer>
+        <nav>
+            <ul>
+                <li> <a href="http://intagram.com"  rel="noopenernoreferrer">IG</a></li>
+            </ul>
+        </nav>
+       <p>
+        Copyright Alkemy challenge
+       </p>
+    </Footer>
+}
+export default Footer;
