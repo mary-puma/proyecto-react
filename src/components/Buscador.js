65bc08af61e930566e745a43ed4adb4be8965b22
Mary <pumaisabel029@gmail.com>
2023-01-03 20:43:56 -0300

    en la page listado colocamos el title en la url

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,19 +1,38 @@
+import sweetalert2 from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 function Buscador() {
-    const submitHandler = e =>{
+
+    const navigate = useNavigate();
+    const submitHandler = e => {
         e.preventDefault(); // para que no reenderice la pagina al hacer click en el boton buscar
-        const keyword = e.currentTarget.keyword.value; //capturamos el input de buscador, trim() no contabiliza los espacios iniciales ni finales
-       
+        const keyword = e.currentTarget.keyword.value.trim(); //capturamos el input de buscador, trim() no contabiliza los espacios iniciales ni finales
+
+        console.log(keyword);
+        if (keyword.length === 0) {
+            sweetalert2.fire({
+                html: '<h3>Tenes que escribir una palabra clave</h3>'
+            })
+           
+        }else if (keyword.length < 4){
+            sweetalert2.fire({
+                html: '<h3>Escribir más de 3 caracteres</h3>'
+            })
+        } else {
+            navigate(`/listado?title=${keyword}`);//redireccionamos a listado con la palabra clave que colocamos en el buscador
+            
+        }
+
     }
     return (
-       
+
         <form onSubmit={submitHandler}>
             <div className="d-flex justify-content-center" >
                 <label className="form-label mb-0 mx-2">
-                <input type="text" classame="form-control" name="keyword" />
+                    <input type="text" classame="form-control" name="keyword" />
                 </label>
                 <button type="submit" className="btn btn-primary" >Buscar</button>
             </div>
-           
+
         </form>
     )
 
