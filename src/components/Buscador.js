baa73f7395c98ca2e4588cf7e1f166e98341ad73
Mary <pumaisabel029@gmail.com>
2023-01-04 21:50:00 -0300

    usamos searchParams para controlar el render

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,13 +1,12 @@
 import sweetalert2 from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 function Buscador() {
-
+    
     const navigate = useNavigate();
     const submitHandler = e => {
         e.preventDefault(); // para que no reenderice la pagina al hacer click en el boton buscar
         const keyword = e.currentTarget.keyword.value.trim(); //capturamos el input de buscador, trim() no contabiliza los espacios iniciales ni finales
-
-        console.log(keyword);
+        
         if (keyword.length === 0) {
             sweetalert2.fire({
                 html: '<h3>Tenes que escribir una palabra clave</h3>'
@@ -18,10 +17,11 @@ function Buscador() {
                 html: '<h3>Escribir más de 3 caracteres</h3>'
             })
         } else {
-            e.currentTarget.keyword.value = '';
-            navigate(`/listado?title=${keyword}`);//redireccionamos a listado con la palabra clave que colocamos en el buscador
-            
-        }
+            //e.currentTarget.keyword.value = '';
+            navigate(`/listado?keyword=${keyword}`);//redireccionamos a listado con la palabra clave que colocamos en el buscador
+                
+
+    }
 
     }
     return (
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, Link, Navigate } from 'react-router-dom';
+import { useNavigate, Link, Navigate, useSearchParams} from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
 import Buscador from './Buscador';
@@ -17,20 +17,31 @@ function Listado() {
 
 
     let token = localStorage.getItem('token');
-
+   
     const [movieList, setMovieList] = useState([]);
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    console.log(searchParams.get("keyword"));
 
     useEffect(() => {
-        const endpoint = 'https://omdbapi.com/?i=tt3896198&apikey=7a5b7d31&s=batman'
+
+        //console.log(searchParams.get("keyword"));
+        
+        const endpoint = `https://omdbapi.com/?apikey=7a5b7d31&s=${searchParams.get("keyword")}`;
         axios.get(endpoint)
             .then(response => {
                 const apiData = response.data;
                 setMovieList(apiData.Search);//Search atributo donde estan las peliculas
             })
-    }, [setMovieList]);
+            .catch(error=>{
+                console.log(error);
+            }
+                )
+
+    }, [searchParams]);
 
     console.log(movieList);
-
+  
 
     if (token === null)
         return <Navigate to='/' />
