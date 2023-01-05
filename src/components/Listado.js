f9619a0a1279db2aad3903d190fe0ebcf320cfd4
Mary <pumaisabel029@gmail.com>
2023-01-05 00:53:13 -0300

    colocamos por defecto que se muestre una pelicula

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, Link, Navigate, useSearchParams} from 'react-router-dom';
+import { useNavigate, Link, Navigate, useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import axios from 'axios';
 import Buscador from './Buscador';
@@ -17,7 +17,7 @@ function Listado() {
 
 
     let token = localStorage.getItem('token');
-   
+
     const [movieList, setMovieList] = useState([]);
     let [searchParams, setSearchParams] = useSearchParams();
 
@@ -26,22 +26,25 @@ function Listado() {
     useEffect(() => {
 
         //console.log(searchParams.get("keyword"));
-        
-        const endpoint = `https://omdbapi.com/?apikey=7a5b7d31&s=${searchParams.get("keyword")}`;
+
+        let key = searchParams.get("keyword")
+        if (key === null)
+            key = 'batman'
+        const endpoint = `https://omdbapi.com/?apikey=7a5b7d31&s=${key}`;
         axios.get(endpoint)
             .then(response => {
                 const apiData = response.data;
                 setMovieList(apiData.Search);//Search atributo donde estan las peliculas
             })
-            .catch(error=>{
+            .catch(error => {
                 console.log(error);
             }
-                )
+            )
 
     }, [searchParams]);
 
     console.log(movieList);
-  
+
 
     if (token === null)
         return <Navigate to='/' />
