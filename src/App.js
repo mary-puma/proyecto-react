55d86a3b89d2abc5be174331add8b10f51592540
Mary <pumaisabel029@gmail.com>
2023-01-06 09:51:07 -0300

    agregar o eliminar las movies en el localStorage

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ import './css/footer.css'
 
 function App() {
 
+
   const addOrRemoveFromFav = e => {
+
+    const favMovies = localStorage.getItem('favs');
+    let tempMoviesInFav;
+
+    if (favMovies === null) {
+      tempMoviesInFav = [];
+    } else {
+      tempMoviesInFav = JSON.parse(favMovies);
+    }
+    console.log(tempMoviesInFav);
 
     const btn = e.currentTarget;//capturamos el boton
     const parent = btn.parentElement;//capturamos el padre donde esta el boton
@@ -22,24 +33,40 @@ function App() {
     const overview = parent.querySelector('p').innerText;
     const movieData = {
       imgUrl,
-      title, 
+      title,
       overview,
       id: btn.dataset.movieId
     }
 
+    let moviesIsInArray = tempMoviesInFav.find(oneMovie => {
+      return oneMovie.id === movieData.id
+    });
+
+    if (!moviesIsInArray) {
+      tempMoviesInFav.push(movieData);
+      localStorage.setItem('favs', JSON.stringify(tempMoviesInFav));
+      console.log('se agrego la pelicula');
+    } else {
+      let moviesLeft = tempMoviesInFav.filter(oneMovie => {
+        return oneMovie.id !== movieData.id
+      });
+      localStorage.setItem('favs', JSON.stringify(moviesLeft));
+      console.log('se elimino la pelicula');
+    }
+
     //console.log(imgUrl);
-    console.log(btn.dataset);
-    console.log(movieData);
+    //console.log(btn.dataset);
+    //console.log(movieData);
 
   }
   return (
     <div className='body'>
       <Header />
       <div className='container mt-3'>
-      <Routes>
-        <Route exact path='/' element={<Login />} />
-        <Route path='/listado' element={<Listado addOrRemoveFromFav={addOrRemoveFromFav} />}  />
-      </Routes>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route path='/listado' element={<Listado addOrRemoveFromFav={addOrRemoveFromFav} />} />
+        </Routes>
       </div>
 
       <Footer />
