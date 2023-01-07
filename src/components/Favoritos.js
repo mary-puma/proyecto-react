b8e4060ee0850fc1d974ec9032c163dd97136959
Mary <pumaisabel029@gmail.com>
2023-01-07 13:12:15 -0300

    logica para eliminar peliculas de favoritos

--- src/components/Favoritos.js
+++ src/components/Favoritos.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react"; 
 import { Link } from "react-router-dom";
-function Favoritos() {
+function Favoritos({favorites,addOrRemoveFromFav}) {
 
-    const [favorites, setFavorites] = useState([]);
+    /*const [favorites, setFavorites] = useState([]);
     useEffect(()=>{
         const favsInLocal = localStorage.getItem('favs');
 
@@ -11,31 +10,34 @@ function Favoritos() {
             console.log(favsArray);
             setFavorites(favsArray);
         }
-    },[])
+    },[])*/
     return (
         <>
-        <h5>Seccion Favoritos</h5>
-        <div className="row">
-        {
-            favorites.map((oneMovie, idx) => {
-                return (
-                    <div className="col-3" key={idx}>
-                        <div className='card my-4'>
-                            <img src={oneMovie.imgUrl} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">{oneMovie.title}</h5>
-                                <p className="card-text">{oneMovie.overview}</p>
-                                
-                                <Link href="/" className="btn btn-primary">Go somewhere</Link>
+            <h5>Seccion Favoritos</h5>
+            <div className="row">
+                {
+                    favorites.map((oneMovie, idx) => {
+                        return (
+                            <div className="col-3" key={idx}>
+                                <div className='card my-4'>
+                                    <img src={oneMovie.imgUrl} className="card-img-top" alt="..." />
+                                    <button className='favourite-btn' onClick={addOrRemoveFromFav} data-movie-id={oneMovie.id}>
+                                        ❤️
+                                    </button>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{oneMovie.title}</h5>
+                                        <p className="card-text">{oneMovie.overview}</p>
 
+                                        <Link href="/" className="btn btn-primary">Go somewhere</Link>
+
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                )
-            })
-        }
-    </div>
-    </>
+                        )
+                    })
+                }
+            </div>
+        </>
 
     )
 
