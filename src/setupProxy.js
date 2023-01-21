524627854fbf3141480479e6c39adceb1b28325e
Mary <pumaisabel029@gmail.com>
2023-01-20 22:45:07 -0300

    config proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+    
+module.exports = function(app) {
+
+    app.use(
+    createProxyMiddleware({
+      target:'http://challenge-react.alkemy.org', 
+      changeOrigin: true,
+    })
+  );
+
+  
+};
