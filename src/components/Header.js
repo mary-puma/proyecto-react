b878dfbd328b3327a5d1499c11caa346b266ec08
mary <pumaisabel029@gmail.com>
2024-12-07 18:54:42 -0300

    cambio de color del boton logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,38 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../auth/context/AuthContext';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../auth/context/AuthContext";
 function Header() {
-
-    const {handlerLogout,login} = useContext(AuthContext);
-    return (
-        <header>
-            <nav>
-                <ul className="nav justify-content-end p-3 mb-2 bg-primary ">
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to={'/'}>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white" to={'/listado'}>Listado</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link text-white " to={'/favoritos'}>Favoritos</Link>
-                    </li>
-                    <li className="nav-item">
-                        <button 
-                        onClick={handlerLogout}
-                        className='btn btn-outline-success'>
-                        Logout
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-
+  const { handlerLogout, login } = useContext(AuthContext);
+  return (
+    <header>
+      <nav>
+        <ul className="nav justify-content-end p-3 mb-2 bg-primary ">
+          <li className="nav-item">
+            <Link className="nav-link text-white" to={"/"}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to={"/listado"}>
+              Listado
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white " to={"/favoritos"}>
+              Favoritos
+            </Link>
+          </li>
+          <li className="nav-item">
+            <button
+              onClick={handlerLogout}
+              className='btn btn-outline-success green-light-btn"'
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 export default Header;
